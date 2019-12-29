3df6e5bbe51f754d82139769411af31b87765ca6
Richard <strunk876@gmail.com>
2019-12-29 03:54:32 +0100

    Remove stupid file header

--- src/de/jpx3/ips/connect/bukkit/IPacketSubscriber.java
+++ src/de/jpx3/ips/connect/bukkit/IPacketSubscriber.java
@@ -3,12 +3,6 @@ package de.jpx3.ips.connect.bukkit;
 import de.jpx3.ips.connect.bukkit.protocol.Packet;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
-/**
- * Class generated using IntelliJ IDEA
- * Any distribution is strictly prohibited.
- * Copyright Richard Strunk 2019
- */
-
 public interface IPacketSubscriber<P extends Packet> {
   void handle(ProxiedPlayer sender, P packet);
 }
