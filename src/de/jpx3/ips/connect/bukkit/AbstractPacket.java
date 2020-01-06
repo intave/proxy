abc1ac78ac7eee6d9591444fe237681177b247ee
Richard <strunk876@gmail.com>
2020-01-06 03:14:44 +0100

    Remove empty line

--- src/de/jpx3/ips/connect/bukkit/AbstractPacket.java
+++ src/de/jpx3/ips/connect/bukkit/AbstractPacket.java
@@ -5,6 +5,5 @@ import com.google.common.io.ByteArrayDataOutput;
 
 public abstract class AbstractPacket {
   public abstract void applyFrom(ByteArrayDataInput input) throws IllegalStateException;
-
   public abstract void applyTo(ByteArrayDataOutput output);
 }
